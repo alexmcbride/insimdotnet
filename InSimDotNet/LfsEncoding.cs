d02e17cdd8e529b5b66a0237051d78094d060998
Alex McBride <xandermcbride@gmail.com>
2016-02-07 14:29:44 +0000

    Reverted LfsEncoding class back to original. Will create fork for new string handling code.

--- InSimDotNet/LfsEncoding.cs
+++ InSimDotNet/LfsEncoding.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
+using System.Linq;
 
 namespace InSimDotNet {
     /// <summary>
     /// Handles converting strings from LFS encoding into unicode and vice versa.
     /// </summary>
-    public static class LfsEncoding {
+    internal static class LfsEncoding {
         private const char ControlChar = '^';
         private const char FallbackChar = '?';
         private static readonly bool IsRunningOnMono = (Type.GetType("Mono.Runtime") != null);
@@ -29,6 +30,19 @@ namespace InSimDotNet {
 
         private static readonly Encoding DefaultEncoding = EncodingMap['L'];
 
+        private static readonly Dictionary<char, char> EscapeMap = new Dictionary<char, char> {
+            { 'v', '|' },
+            { 'a', '*' },
+            { 'c', ':' },
+            { 'd', '\\' },
+            { 's', '/' },
+            { 'q', '?' },
+            { 't', '"' },
+            { 'l', '<' },
+            { 'r', '>' },
+            { '^', '^' },
+        };
+
         /// <summary>
         /// Converts a LFS encoded string to unicode.
         /// </summary>
@@ -38,35 +52,142 @@ namespace InSimDotNet {
         /// <returns>The resulting unicode string.</returns>
         public static string GetString(byte[] buffer, int index, int length) {
             StringBuilder output = new StringBuilder(length);
-            Encoding encoding = DefaultEncoding, nextEncoding;
-            int i = 0, lastEncode = index;
-            char control, nextChar;
+            Encoding encoding = DefaultEncoding;
+            Encoding nextEncoding;
+            int i = 0, start = index;
+            char escape;
 
             for (i = index; i < index + length; i++) {
-                control = (char)buffer[i];
+                char control = (char)buffer[i];
 
                 // Check for null terminator.
                 if (control == Char.MinValue) {
                     break;
                 }
 
-                // Check if codepage switch needed.
-                if (control == ControlChar && (i + 1) < buffer.Length) {
-                    nextChar = (char)buffer[i + 1];
-                    if (EncodingMap.TryGetValue(nextChar, out nextEncoding)) {
-                        output.Append(encoding.GetString(buffer, lastEncode, (i - lastEncode)));
-                        lastEncode = i;
-                        encoding = nextEncoding;
+                // If not control character then ignore.
+                if (control != ControlChar) {
+                    continue;
+                }
+
+                // Found control character so encode everything up to this point.
+                if (i - start > 0) {
+                    output.Append(encoding.GetString(buffer, start, (i - start)));
+                }
+                start = (i + 2); // skip control chars.
+
+                // Process control character.
+                char next = (char)buffer[++i];
+                if (EncodingMap.TryGetValue(next, out nextEncoding)) {
+                    encoding = nextEncoding; // Switch encoding.
+                }
+                else if (EscapeMap.TryGetValue(next, out escape)) {
+                    output.Append(escape); // Escape character.
+                }
+                else {
+                    // Character not codepage switch or escape, so just ignore it.
+                    output.Append(control);
+                    output.Append(next);
+                }
+            }
+
+            // End of string reached so encode up all to this point.
+            if (i - start > 0) {
+                output.Append(encoding.GetString(buffer, start, (i - start)));
+            }
+
+            return output.ToString();
+        }
+
+        /// <summary>
+        /// Gets the number of bytes a string will take up once it has been converted from unicode string into a LFS encoded string.
+        /// </summary>
+        /// <param name="value">The string to test.</param>
+        /// <param name="maxLength">The maximum number of characters to test.</param>
+        /// <returns>The resulting number of bytes.</returns>
+        public static int GetByteCount(string value, int maxLength) {
+            Encoding encoding = DefaultEncoding;
+            int tempCount, result = 0;
+
+            for (int i = 0; i < value.Length && i < maxLength; i++) {
+                if (value[i] <= 127) {
+                    // All codepages share first 128 values.
+                    result++;
+                }
+                else if (TryGetByteCount(encoding, value[i], out tempCount)) {
+                    // Charcter in current codepage.
+                    result += tempCount;
+                }
+                else {
+                    // Search for new codepage.
+                    bool found = false;
+
+                    foreach (KeyValuePair<char, Encoding> map in EncodingMap) {
+                        if (map.Value == encoding) {
+                            continue;
+                        }
+
+                        if (TryGetByteCount(map.Value, value[i], out tempCount)) {
+                            encoding = map.Value;
+                            result += tempCount + 2;
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        result++;
                     }
                 }
             }
 
-            // Encode anything that's left.
-            if (i - lastEncode > 0) {
-                output.Append(encoding.GetString(buffer, lastEncode, (i - lastEncode)));
+            return result;
+        }
+
+        /// <summary>
+        /// Tries to figure out the number of bytes a unicode character will take up once it has been converted into a LFS character. Either one or two bytes.
+        /// </summary>
+        /// <param name="encoding">The encoding to try and convert the character to.</param>
+        /// <param name="value">The character to convert.</param>
+        /// <param name="count">The numbers of bytes the character will take up.</param>
+        /// <returns>Returns true if the character could be successfully converted or false otherwise.</returns>
+        [DebuggerStepThrough]
+        private static bool TryGetByteCount(Encoding encoding, char value, out int count) {
+            // We use WideCharToMultiByte on Windows as it's very fast, but that's not 
+            // available on Mono so we revert to trying to convert a character and then 
+            // catching the exception generated when it fails. This is very slow as the 
+            // callstack may potentionally need to be unwound for every character in the 
+            // string.
+            if (IsRunningOnMono) {
+                return TryGetByteCountMono(encoding, value, out count);
             }
 
-            return output.ToString();
+            return TryGetByteCountDotNet(encoding, value, out count);
+        }
+
+        private static bool TryGetByteCountDotNet(Encoding encoding, char value, out int count) {
+            bool usedDefault = false;
+            count = NativeMethods.WideCharToMultiByte(
+                (uint)encoding.CodePage,
+                NativeMethods.WC_NO_BEST_FIT_CHARS,
+                value.ToString(),
+                1,
+                null,
+                0,
+                IntPtr.Zero,
+                out usedDefault);
+            return !usedDefault;
+        }
+
+        private static bool TryGetByteCountMono(Encoding encoding, char value, out int count) {
+            try {
+                count = encoding.GetByteCount(value.ToString());
+                return true;
+            }
+            catch (EncoderFallbackException) {
+                count = 0;
+                return false;
+            }
         }
 
         /// <summary>
@@ -78,16 +199,29 @@ namespace InSimDotNet {
         /// <param name="length">The maximum number of bytes to write.</param>
         /// <returns>The number of bytes written during the operation.</returns>
         public static int GetBytes(string value, byte[] buffer, int index, int length) {
-            Encoding encoding = DefaultEncoding, tempEncoding;
+            Encoding encoding = DefaultEncoding;
             byte[] tempBytes = new byte[2];
-            int tempCount, start = index, totalLength = index + (length - 1);
-            bool found;
+            int tempCount;
+            int start = index;
+            int totalLength = index + (length - 1);
 
             for (int i = 0; i < value.Length && index < totalLength; i++) {
-                // Switch codepage.
-                if (value[i] == '^' && i + 1 < value.Length) {
-                    if (EncodingMap.TryGetValue(value[i + 1], out tempEncoding)) {
-                        encoding = tempEncoding;
+                // Remove any existing language tags from the string.
+                int next = i + 1;
+                if (value[i] == '^' && next < value.Length) {
+                    switch (value[next]) {
+                        case 'L':
+                        case 'G':
+                        case 'C':
+                        case 'J':
+                        case 'E':
+                        case 'T':
+                        case 'B':
+                        case 'H':
+                        case 'S':
+                        case 'K':
+                            i++; // skip codepage chars
+                            continue;
                     }
                 }
 
@@ -102,7 +236,7 @@ namespace InSimDotNet {
                 }
                 else {
                     // Search for new codepage.
-                    found = false;
+                    bool found = false;
                     foreach (KeyValuePair<char, Encoding> map in EncodingMap) {
                         if (map.Value == encoding) {
                             continue; // Skip current as we've already searched it.
