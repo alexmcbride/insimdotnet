c3151ac0779e0af7fc59b6319dd426d4d29e6990
Alex McBride <xandermcbride@gmail.com>
2016-02-09 20:58:17 +0000

    Progress on BatchHelper - still experimental and not to be used

--- InSimDotNet/Helpers/BatchHelper.cs
+++ InSimDotNet/Helpers/BatchHelper.cs
@@ -2,44 +2,22 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Threading.Tasks;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace InSimDotNet.Helpers {
-    /// <summary>
-    /// Class to help sending large amounts of packets.
-    /// </summary>
-    /// <remarks>
-    /// If you flood InSim with too many packets it will cause an error. This class helps that. It tracks 
-    /// how many bytes you send to LFS and inserts a delay if you send too many over a specified period.
-    /// </remarks>
     public class BatchHelper {
         private InSim insim;
-        private int bytesSent;
-        private bool isPaused;
         private List<ISendable> buffer = new List<ISendable>();
+        private bool isAlreadySending;
 
-        /// <summary>
-        /// Gets the underlying InSim object.
-        /// </summary>
-        public InSim InSim {
-            get { return insim; }
+        public int BatchSize { get; set; }
+        public int BatchDelay { get; private set; }
+
+        public bool IsConnected {
+            get { return insim.IsConnected; }
         }
 
-        /// <summary>
-        /// Gets or sets the max number of bytes to send without delaying.
-        /// </summary>
-        public int BatchBytes { get; set; }
-
-        /// <summary>
-        /// Gets or sets the number of seconds to delay sending for.
-        /// </summary>
-        public int BatchDelay { get; set; }
-
-        /// <summary>
-        /// Creates a new BatchHelper object.
-        /// </summary>
-        /// <param name="insim">The InSim object to use when sending packets.</param>
         public BatchHelper(InSim insim) {
             if (insim == null) {
                 throw new ArgumentNullException("insim");
@@ -47,81 +25,71 @@ namespace InSimDotNet.Helpers {
 
             this.insim = insim;
 
-            BatchBytes = 1024;
-            BatchDelay = 100;
-
-            insim.Initialized += insim_Initialized;
-
-            if (insim.IsConnected) {
-                RunAsync();
-            }
+            BatchSize = 2048; // bytes
+            BatchDelay = 100; // milliseconds
         }
 
-        /// <summary>
-        /// Dispose the object.
-        /// </summary>
-        public void Dispose() {
-            insim.Initialized -= insim_Initialized;
-        }
-
-        private void insim_Initialized(object sender, InitializeEventArgs e) {
-            buffer.Clear();
-
-            RunAsync();
-        }
-
-        private async void RunAsync() {
+        public void Send(ISendable packet) {
             if (!insim.IsConnected) {
+                throw new InSimException(StringResources.InSimNotConnectedMessage);
+            }
+
+            buffer.Add(packet);
+
+            BatchSendAsync();
+        }
+
+        public void Send(params ISendable[] packets) {
+            if (!insim.IsConnected) {
+                throw new InSimException(StringResources.InSimNotConnectedMessage);
+            }
+
+            buffer.AddRange(packets);
+
+            BatchSendAsync();
+        }
+
+        private async void BatchSendAsync() {
+            if (isAlreadySending) {
+                Debug.WriteLine("already sending");
                 return;
             }
 
-            await Task.Delay(BatchDelay);
+            isAlreadySending = true;
 
-            if (isPaused) {
-                insim.Send(buffer.ToArray());
-                buffer.Clear();
+            while (true) {
+                var packets = GetPacketBatch();
 
-                bytesSent = 0;
-                isPaused = false;
+                insim.Send(packets.ToArray());
+
+                // wait and see if any more packets added.
+                await Task.Delay(BatchDelay);
+
+                Debug.WriteLine("just waited "+ BatchDelay + " ms");
+
+                // if not then bye bye
+                if (buffer.Count == 0) {
+                    break;
+                }
             }
 
-            if (bytesSent > BatchBytes) {
-                isPaused = true;
-
-                Debug.WriteLine("batch sending delay kicked in");
-            }
-
-            RunAsync();
+            isAlreadySending = false;
         }
 
-        /// <summary>
-        /// Sends a packet to LFS.
-        /// </summary>
-        /// <param name="packet">The packet to send.</param>
-        public void Send(ISendable packet) {
-            bytesSent += packet.Size;
+        private IList<ISendable> GetPacketBatch() {
+            var packets = new List<ISendable>();
 
-            if (isPaused) {
-                buffer.Add(packet);
+            int size = 0;
+            for (int i = 0; i < buffer.Count; i++) {
+                if (size + buffer[i].Size < BatchSize) {
+                    packets.Add(buffer[i]);
+                    size += buffer[i].Size;
+                    break;
+                }
             }
-            else {
-                insim.Send(packet);
-            }
-        }
+            buffer.RemoveRange(0, packets.Count);
 
-        /// <summary>
-        /// Sends an array of packets to LFS.
-        /// </summary>
-        /// <param name="packets">The array to send.</param>
-        public void Send(params ISendable[] packets) {
-            bytesSent += packets.Sum(p => p.Size);
-
-            if (isPaused) {
-                buffer.AddRange(packets);
-            }
-            else {
-                insim.Send(packets);
-            }
+            return packets;
         }
     }
 }
