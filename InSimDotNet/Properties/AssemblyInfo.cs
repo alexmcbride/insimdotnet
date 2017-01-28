6c0e2fbd790d40db334f3c6d03022c6f7754d616
Alex McBride <xandermcbride@gmail.com>
2017-01-28 19:43:20 +0000

    Updated version file and NuGet package

--- InSimDotNet/Properties/AssemblyInfo.cs
+++ InSimDotNet/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("InSim.NET")]
-[assembly: AssemblyCopyright("Copyright © Alex McBride 2010 - 2015")]
+[assembly: AssemblyCopyright("Copyright © Alex McBride 2010 - 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
@@ -35,6 +35,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.3.1.0")]
+[assembly: AssemblyFileVersion("2.3.1.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
