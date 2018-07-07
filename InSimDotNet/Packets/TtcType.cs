37aeeea1eb9bcb8c162bba477bee06eb6fa33035
Marc Le Bouthillier <chauffretteclimatisee@protonmail.com>
2018-07-07 19:30:21 -0400

    Updated IS_AXM action flags, TTC SubT and PMO flags

--- InSimDotNet/Packets/TtcType.cs
+++ InSimDotNet/Packets/TtcType.cs
@@ -14,5 +14,15 @@
         /// Send IS_AXM for a layout editor selection
         /// </summary>
         TTC_SEL,
+
+        /// <summary>
+        /// Send IS_AXM every time the selection changes
+        /// </summary>
+        TTC_SEL_START,
+
+        /// <summary>
+        /// Switch off IS_AXM requested by TTC_SEL_START
+        /// </summary>
+        TTC_SET_STOP
     }
 }
