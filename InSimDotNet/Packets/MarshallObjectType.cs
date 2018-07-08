17d77dc0d82f898d463c25e3641c343b41bcdeec
Marc Le Bouthillier <chauffretteclimatisee@protonmail.com>
2018-07-07 20:01:54 -0400

    IS_CIM packet class and its corresponding enums

---
+++ InSimDotNet/Packets/MarshallObjectType.cs
@@ -0,0 +1,27 @@
+﻿namespace InSimDotNet.Packets {
+    /// <summary>
+    /// Some of the object type values to represent the IS_CIM SelType attribute.
+    /// The remaining is found in the OCOIndex enumeration.
+    /// </summary>
+    public enum MarshallObjectType {
+        /// <summary>
+        /// InSim checkpoint.
+        /// </summary>
+        MARSH_IS_CP = 252,
+
+        /// <summary>
+        /// InSim circle.
+        /// </summary>
+        MARSH_IS_AREA = 253,
+
+        /// <summary>
+        /// Restricted area.
+        /// </summary>
+        MARSH_MARSHALL = 254,
+
+        /// <summary>
+        /// Route checker.
+        /// </summary>
+        MARSH_ROUTE = 255
+    }
+}
