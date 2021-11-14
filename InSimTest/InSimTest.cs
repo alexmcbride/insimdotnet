ea68c79551866a49b45c9b3d9c7e111819c2548b
Gedas <xspeedasx@gmail.com>
2021-11-14 10:25:01 +0200

    Cleaned up test project, updated version

--- InSimTest/InSimTest.cs
+++ InSimTest/InSimTest.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace InSimTest
 {
-    class Program
+    class InSimTest
     {
         static void Main(string[] args)
         {
-            new Program().RunAsync().Wait();
+            new InSimTest().RunAsync().Wait();
         }
 
         private async Task RunAsync()
@@ -19,8 +19,9 @@ namespace InSimTest
             {
                 client.IS_VER += Client_IS_VER;
                 client.IS_MSO += Client_IS_MSO;
+                // example of async event handling
                 client.IS_NPL += async (o, e) => { await Client_IS_NPL(o, e); };
-                client.IS_MAL += async (o,e) => { await Client_IS_MAL(o, e); };
+                client.IS_MAL += async (o, e) => { await Client_IS_MAL(o, e); };
 
                 await client.InitializeAsync(new InSimSettings
                 {
@@ -29,6 +30,7 @@ namespace InSimTest
                 });
 
                 await client.SendAsync(new IS_MAL(
+                    // whitelist allowed mod skinIDs - empty List - all mods allowed
                     new List<string>
                     {
                         //"3118BF",
@@ -39,12 +41,12 @@ namespace InSimTest
                 Console.ReadKey(true);
             }
         }
-        
+
         private async Task Client_IS_NPL(object o, PacketEventArgs<IS_NPL> e)
         {
             var insim = (InSimClient)o;
             var npl = e.Packet;
-            await insim.SendAsync($"^7Player is joining with ^3{npl}");
+            await insim.SendAsync($"^7Player is joining with ^3{npl.CName}");
         }
 
         private async Task Client_IS_MAL(object sender, PacketEventArgs<IS_MAL> e)
@@ -55,9 +57,11 @@ namespace InSimTest
             if (mal.NumM == 0) await insim.SendAsync("^7Host allows ^3ALL ^7mods");
             else
             {
-                foreach (var skinID in mal.SkinIDs) 
+                var cnt = 0;
+                await insim.SendAsync("^7Host allows these mods:");
+                foreach (var skinID in mal.SkinIDs)
                 {
-                    await insim.SendAsync($"^7Host allows: ^3{skinID.StringForm}");
+                    await insim.SendAsync($"^7[{++cnt}/{mal.SkinIDs.Count}]: ^3{skinID.StringForm}");
                 }
             }
         }
@@ -68,7 +72,7 @@ namespace InSimTest
         }
 
         private void Client_IS_VER(object sender, PacketEventArgs<IS_VER> e)
-        { 
+        {
             Console.WriteLine("InSim connected! " + e.Packet.InSimVer);
         }
     }
