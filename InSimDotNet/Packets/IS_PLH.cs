c2b3e7ec1a3d4811236859afb45bf6d969219aee
Kipieslim <8994199+kipieslim@users.noreply.github.com>
2025-01-15 17:48:42 +0100

    Added IS_PLH packet support.

---
+++ InSimDotNet/Packets/IS_PLH.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InSimDotNet.Packets
+{
+    public class IS_PLH : IPacket, ISendable
+    {
+        private const int MAX_PLH_PLAYERS = 40;
+        /// <summary>
+        /// Gets the packet size.
+        /// </summary>
+        public int Size { get; private set; }
+
+        /// <summary>
+        /// Gets the packet type.
+        /// </summary>
+        public PacketType Type { get; private set; }
+
+        /// <summary>
+        /// Gets the request ID.
+        /// </summary>
+        /// <remarks>
+        /// 0 unless this is a reply to a TINY_PLH request
+        /// </remarks>
+        public byte ReqI { get; set; }
+
+        /// <summary>
+        /// Gets the number of players in this packet
+        /// </summary>
+        public byte NumP { get; set; }
+
+        /// <summary>
+        /// Gets a collection of <see cref="PlayerHCap"/> sub-packets.
+        /// </summary>
+        public IList<PlayerHCap> HCaps { get; private set; }
+
+        /// <summary>
+        /// Create a new <see cref="IS_PLH"/> object.
+        /// </summary>
+        public IS_PLH()
+        {
+            Size = 4;
+            Type = PacketType.ISP_PLH;
+            HCaps = new List<PlayerHCap>(MAX_PLH_PLAYERS);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="IS_PLH"/> object.
+        /// </summary>
+        /// <param name="hcap">A collection of <see cref="PlayerHCap"/> sub-packets</param>
+        public IS_PLH(IEnumerable<PlayerHCap> hcap)
+            : this()
+        {
+            HCaps = new List<PlayerHCap>(hcap);
+            Console.WriteLine(HCaps.Count);
+            NumP = (byte)HCaps.Count;
+            Size = 4 + (NumP * 4);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="IS_PLH"/> object.
+        /// </summary>
+        /// <param name="buffer">The packet data</param>
+        public IS_PLH(byte[] buffer)
+        {
+            PacketReader reader = new PacketReader(buffer);
+            Size = reader.ReadSize();
+            Type = (PacketType)reader.ReadByte();
+            ReqI = reader.ReadByte();
+            NumP = reader.ReadByte();
+            HCaps = new List<PlayerHCap>(NumP);
+
+            for (int i = 0; i < NumP; i++)
+            {
+                HCaps.Add(new PlayerHCap(reader));
+            }
+        }
+
+        /// <summary>
+        /// Gets the packet data.
+        /// </summary>
+        /// <returns>An array containing the packet data.</returns>
+        public byte[] GetBuffer()
+        {
+            if (NumP > MAX_PLH_PLAYERS)
+                throw new InvalidOperationException("IS_PLH too many player handicaps set");
+
+            Size = 4 + (HCaps.Count * 4);
+            Console.WriteLine($"Size set to {Size} with {HCaps.Count}");
+            PacketWriter writer = new PacketWriter(Size);
+            writer.WriteSize(Size);
+            writer.Write((byte)Type);
+            writer.Write(ReqI);
+            writer.Write(NumP);
+            foreach (PlayerHCap hcap in HCaps)
+            {
+                hcap.GetBuffer(writer);
+            }
+            var output = writer.GetBuffer();
+            Console.WriteLine($"Actual Size was {output.Length} and the size byte was {output[0]}");
+            return output;
+        }
+    }
+}
---
+++ InSimDotNet/Packets/PlayerHCap.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace InSimDotNet.Packets
+{
+    /// <summary>
+    /// Player handicaps - there is an array of these in IS_PLH.
+    /// </summary>
+    public class PlayerHCap
+    {
+        /// <summary>
+        /// Player's unique ID
+        /// </summary>
+        public byte PLID { get; set; }
+
+        /// <summary>
+        /// Handicap flags
+        /// </summary>
+        public PlayerHCapFlag Flags { get; set; }
+
+        /// <summary>
+        /// Added mass (0kg to 200kg)
+        /// </summary>
+        public byte H_Mass { get; set; }
+
+        /// <summary>
+        /// Intake restriction (0 to  50)
+        /// </summary>
+        public byte H_TRes { get; set; }
+
+        /// <summary>
+        /// Creates a new <see cref="PlayerHCap"/> object.
+        /// </summary>
+        public PlayerHCap(){}
+
+        /// <summary>
+        /// Creates a new PlayerHCap object.
+        /// </summary>
+        /// <param name="reader">A <see cref="PacketReader"/> containing packet data</param>
+        public PlayerHCap(PacketReader reader)
+        {
+            if(reader == null)
+                throw new ArgumentNullException("reader");
+
+            PLID = reader.ReadByte();
+            Flags = (PlayerHCapFlag)reader.ReadByte();
+            H_Mass = reader.ReadByte();
+            H_TRes = reader.ReadByte();
+        }
+
+        /// <summary>
+        /// Writes the <see cref="PlayerHCap"/> object to the specified <see cref="PacketWriter"/>.
+        /// </summary>
+        /// <param name="writer">The <see cref="PacketWriter"/> to write the data to.</param>
+        public void GetBuffer(PacketWriter writer)
+        {
+            if (writer == null)
+                throw new ArgumentNullException("writer");
+
+            writer.Write(PLID);
+            writer.Write((byte)Flags);
+            writer.Write(H_Mass);
+            writer.Write(H_TRes);
+        }
+    }
+}
